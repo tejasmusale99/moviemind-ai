074fff3fa271a52250327bbd3012885e5cb86187
Tejas Musale <tejasmusale99@gmail.com>
2026-03-10 00:03:49 +0530

    updated hero jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import background from '../assets/Front Background image.jpg'
+import EmailSignup from './EmailSignup'
 
 const Hero = () => {
   return (
@@ -6,13 +7,14 @@ const Hero = () => {
       <img src={background} alt="background" className='absolute w-full h-full object-cover'/>
 
       <div className='absolute inset-0 bg-black/60 flex flex-col justify-center items-center text-center px-4'>
-        <h1 className='text-5xl font-bold mb-4'>Unlimited movies, shows, and more</h1>
+        <h1 className='text-6xl font-bold mb-4'>Unlimited movies, <br />shows, and more</h1>
         <p className='text-xl mb-2'>
           Starts at ₹149. Cancel anytime.
         </p>
         <p className='mb-4'>
           Ready to watch? Enter your email to create or restart your membership.
         </p>
+        <EmailSignup />
       </div>
     </div>
   )
