bcba7961ec6dae19044b8b60762f151259a3b5bf
Tejas Musale <tejasmusale99@gmail.com>
2026-03-10 00:03:20 +0530

    created email signup component

--- src/components/EmailSignup.jsx
+++ src/components/EmailSignup.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const EmailSignup = () => {
   return (
-    <div>EmailSignup</div>
-  )
-}
+    <div className="flex gap-2">
+      <input
+        type="email"
+        placeholder="Email address"
+        className="p-3 w-80 rounded bg-black bg-opacity-60 border border-gray-400"
+      />
+      <button className="bg-red-600 px-6 py-3 rounded font-bold">Get Started</button>
+    </div>
+  );
+};
 
-export default EmailSignup
+export default EmailSignup;
