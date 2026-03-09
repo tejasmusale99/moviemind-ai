b8996b2d84ce10ee008cfcbf9ae7310d6fe86a61
Tejas Musale <tejasmusale99@gmail.com>
2026-03-09 23:36:19 +0530

    Built and style header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
+import logo from "../assets/Netflix_Logo_PMS.png"
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="w-full px-10 py-5 flex justify-between items-center z-20">
+        <img src={logo} alt="logo" className="w-36 cursor-pointer" />
+
+        <div className="flex items-center gap-4">
+            <select className="bg-black text-white border border-gray-500 px-3 py-1 rounded">
+                <option>English</option>
+                <option>हिंदी</option>
+            </select>
+            <button className="bg-red-600 px-4 py-2 rounded text-white font-semibold">Sign In</button>
+        </div>
+    </div>
   )
 }
 
