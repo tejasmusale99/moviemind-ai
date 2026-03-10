3ee555318654fa1edc4c4eb45068f95787780239
Tejas Musale <tejasmusale99@gmail.com>
2026-03-10 21:33:46 +0530

    update header with Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import logo from "../assets/Netflix_Logo_PMS.png"
-const Header = () => {
+import { Link } from "react-router-dom";
+import logo from "../assets/Netflix_Logo_PMS.png";
+const Header = () => { 
   return (
     <div className="absolute w-full px-10 py-5 flex justify-between items-center z-20">
-        <img src={logo} alt="logo" className="w-36 cursor-pointer" />
+      <Link to={"/"}><img src={logo} alt="logo" className="w-36 cursor-pointer" /></Link>
 
-        <div className="flex items-center gap-4">
-            <select className="bg-black text-white border border-gray-500 px-3 py-1 rounded">
-                <option>English</option>
-                <option>हिंदी</option>
-            </select>
-            <button className="bg-red-600 px-4 py-2 rounded text-white font-semibold">Sign In</button>
-        </div>
+      <div className="flex items-center gap-4">
+        <select className="bg-black text-white border border-gray-500 px-3 py-1 rounded">
+          <option>English</option>
+          <option>हिंदी</option>
+        </select>
+        <button className="bg-red-600 px-4 py-2 rounded text-white font-semibold">
+          <Link to={"/signin"}>Sign In</Link>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
