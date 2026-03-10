9945f95a69e959ae5f82132a12e43c3c5162db46
Tejas Musale <tejasmusale99@gmail.com>
2026-03-10 20:33:53 +0530

    created a SignIn comonent

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,15 @@
+import backgroundImg from '../assets/Front Background image.jpg'
+import Header from './Header'
+
+const SignIn = () => {
+  return (
+    <div className='relative h-screen text-white'>
+        <img className='absolute w-full h-full object-cover' src={backgroundImg} alt="SignIn_Bg" />
+         <Header />
+
+         <div></div>
+    </div>
+  )
+}
+
+export default SignIn
