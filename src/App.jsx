08eebb7dda265b775109db1b9115b54e7fc25d25
Tejas Musale <tejasmusale99@gmail.com>
2026-03-10 20:49:12 +0530

    configured Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Body from "./components/Body";
+import SignIn from "./components/SignIn";
 
 function App() {
-  return (
-    <div>
-      <Body />
-    </div>
-  );
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+    },
+    {
+      path: "/signin",
+      element: <SignIn />,
+    },
+  ]);
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
