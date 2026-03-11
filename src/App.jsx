7d1fb96849eb034f5745029aed62be7daa6055c9
Tejas Musale <tejasmusale99@gmail.com>
2026-03-11 20:05:07 +0530

    created a signup Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Body from "./components/Body";
 import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -11,6 +12,10 @@ function App() {
     {
       path: "/signin",
       element: <SignIn />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
     },
   ]);
   return <RouterProvider router={appRouter} />;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignIn from './SignIn'
+
+const SignUp = () => {
+  return (
+    <><SignIn /></>
+  )
+}
+
+export default SignUp
