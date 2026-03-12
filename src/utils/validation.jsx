e6c049d402a20e71ae477ec01d0c6e96a34c02fb
Tejas Musale <tejasmusale99@gmail.com>
2026-03-12 21:41:23 +0530

    write regex for validation

---
+++ src/utils/validation.jsx
@@ -0,0 +1,9 @@
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+
+  if (!isEmailValid) return "Email is Not valid";
+  if (!isPasswordValid) return "Password must include uppercase, lowercase, number, special character and be 8+ characters long.";
+
+  return null;
+};
