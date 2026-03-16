2eade12f1c3b6e0cb445ffe93f3146d8c0d25fff
Tejas Musale <tejasmusale99@gmail.com>
2026-03-16 21:43:20 +0530

    setup store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App"
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={appStore}>
     <App />
+    </Provider>
   </StrictMode>,
 )
