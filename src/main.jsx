1527b1c172c9d858324c3e30d1c108fce86e268d
Tejas Musale <tejasmusale99@gmail.com>
2026-03-26 21:06:19 +0530

    updated main js file

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from "./App"
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={appStore}>
+createRoot(document.getElementById("root")).render(
+  <Provider store={appStore}>
     <App />
-    </Provider>
-  </StrictMode>,
-)
+  </Provider>,
+);
