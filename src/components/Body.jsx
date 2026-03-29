7d5736398a5c0cb89d0c8ecc62974a25cf305897
Tejas Musale <tejasmusale99@gmail.com>
2026-03-29 12:09:58 +0530

    Implememted onAuthStateChange for sign and signup

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,36 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import SignIn from "./SignIn";
 // import Hero from "./Hero";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import Browse from "./Browse";
+
 
 const Body = () => {
-  return(
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+  return (
     <div className="appLayout">
-        <Header />
-        <SignIn />
+      <Header />
+      <SignIn />
+      <Browse />
     </div>
-  ) 
+  );
 };
 
 export default Body;
