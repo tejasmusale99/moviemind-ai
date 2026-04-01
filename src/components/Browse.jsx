27683bba82526035fb0947a437c968debf92e7a9
Tejas Musale <tejasmusale99@gmail.com>
2026-04-01 23:41:05 +0530

    store movie data in redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { movieOptions } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+    const dispatch = useDispatch();
+
   const fetchNowPlayingData = async () => {
     const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?&page=1", movieOptions);
     const jsonData = await response.json();
+    dispatch(addMovies(jsonData.results));
     console.log(jsonData);
+
     return <div className="browse"></div>;
   };
 
