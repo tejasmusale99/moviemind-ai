88530f175fa8bce4fce35c9dc8cd766d9d583b31
Tejas Musale <tejasmusale99@gmail.com>
2026-04-01 23:15:52 +0530

    Api call for movie

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { movieOptions } from "../utils/constants";
+
 const Browse = () => {
-    return (
-        <div className="browse">
-            <h1>Browse</h1>
-        </div>
-    );
-}
+  const fetchNowPlayingData = async () => {
+    const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?&page=1", movieOptions);
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return <div className="browse"></div>;
+  };
+
+  useEffect(() => {
+    fetchNowPlayingData();
+  }, []);
+
+  return (
+    <div className="browse">
+      <h1>Browse</h1>
+    </div>
+  );
+};
 
 export default Browse;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
 export const userAvtar = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
+
+// API key for The Movie Database (TMDb)
+export const movieOptions = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzk3NzMyZjgyYjc0Y2E3N2ZmMzMyOTgxMWEzZjc0YyIsIm5iZiI6MTc3NDk2ODMxMS40MTYsInN1YiI6IjY5Y2JkZGY3OWEwMDk0ZTJhMWM3NzQzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PrqamG4Jz0tKLvNETrUV1XOUZQ2zm7We49cMQMBEUVY'
+  }
+};
