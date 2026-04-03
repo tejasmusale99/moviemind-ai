29f326bb9080d23ee4d37e246cba1da8b862f53d
Tejas Musale <tejasmusale99@gmail.com>
2026-04-03 23:25:43 +0530

    layout

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -4,6 +4,16 @@ const Browse = () => {
   useFetchNowPlaying(); 
   return (
     <div className="browse">
+      {/* 
+      
+    MainContainer
+      - VideoBackground
+      - VideoTitle
+    SecondaryContainer
+      - MovieList * n
+        - cards * n  
+      
+      */}
       <h1>Browse</h1>
     </div>
   );
