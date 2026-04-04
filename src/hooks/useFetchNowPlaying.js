5628aa63ed43c58b7a31fefbb1ce11e31655d8aa
Tejas Musale <tejasmusale99@gmail.com>
2026-04-04 21:40:48 +0530

    removed unnecessary logs

--- src/hooks/useFetchNowPlaying.js
+++ src/hooks/useFetchNowPlaying.js
@@ -11,7 +11,7 @@ const useFetchNowPlaying = () => {
     const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?&page=1", movieOptions);
     const jsonData = await response.json();
     dispatch(addNowPlayingMovies(jsonData.results));
-    console.log(jsonData);
+    // console.log(jsonData);
   };
 
   useEffect(() => {
