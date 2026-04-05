e86ca965b28d4c9c43e29bdc9b32608df856e51e
Tejas Musale <tejasmusale99@gmail.com>
2026-04-05 14:13:55 +0530

    style the videotitle

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{overview}</p>
-      <div className='flex'>
-        <button className="bg-white px-4 py-2 rounded text-black font-semibold cursor-pointer text-sm">Play</button>
-        <button className="bg-gray-500 px-4 py-2 rounded text-white font-semibold cursor-pointer text-sm">More Info</button>
+    <div className='px-36 pt-36 space-y-4 absolute z-10 '>
+      <h1 className='text-5xl font-bold'>{title}</h1>
+      <p className='w-1/2'>{overview}</p>
+      <div className='flex gap-3'>
+        <button className="bg-white px-8 py-2 rounded text-black font-semibold cursor-pointer text-sm border">▶️ Play </button>
+        <button className="bg-gray-500 px-4 py-2 rounded text-white font-semibold cursor-pointer text-sm">More Info </button>
       </div>
     </div>
   )
