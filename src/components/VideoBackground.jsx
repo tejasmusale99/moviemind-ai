1707eab506532510ca43c1c055250837bcd02bda
Tejas Musale <tejasmusale99@gmail.com>
2026-04-06 23:29:32 +0530

    fetches video Url data using fetch

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { movieOptions } from '../utils/constants';
 
-const VideoBackground = () => {
+const VideoBackground = ({movieId}) => {
+  console.log(movieId);
+ 
+  const getVideoUrl = async () => {
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`, movieOptions)
+    const jsonData = await data.json();
+    console.log(jsonData);
+  }
+
+  useEffect(()=>{
+    getVideoUrl();
+  },[])
+
   return (
-    <div>VideoBackground</div>
+    <h1>{movieId}</h1>
   )
 }
 
