ef8522c93bc6e0a7dcbbd0e367e6dc3624582e46
Tejas Musale <tejasmusale99@gmail.com>
2026-04-14 11:48:06 +0530

    using redux store fetch dynamic key to show trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
 import { movieOptions } from "../utils/constants";
-import { log } from "firebase/firestore/pipelines";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailer } from "../utils/moviesSlice";
 
 const VideoBackground = ({ movieId }) => {
+  const dispatch = useDispatch();
+  const trailerVideoKey = useSelector((store)=> store.movies.trailer?.key)
   console.log(movieId);
 
   const getMoviesAndVideoUrl = async () => {
@@ -12,17 +15,27 @@ const VideoBackground = ({ movieId }) => {
     );
     const jsonData = await data.json();
     const VideoData = jsonData.results;
-    const filtredData = VideoData.filter((trailer) => trailer.type === "Trailer");
+    const filtredData = VideoData.filter(
+      (trailer) => trailer.type === "Trailer",
+    );
     const trailerVideo = filtredData.length ? filtredData[0] : VideoData[0];
     console.log(trailerVideo);
-    
+    dispatch(addTrailer(trailerVideo))
   };
 
   useEffect(() => {
     getMoviesAndVideoUrl();
   }, []);
 
-  return <h1>{movieId}</h1>;
+  return (
+    <div className="">
+      <iframe
+        src={`https://www.youtube.com/embed/${trailerVideoKey}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
