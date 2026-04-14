7418617b5e37554399dd22106a3438be565e6105
Tejas Musale <tejasmusale99@gmail.com>
2026-04-14 12:58:18 +0530

    fix styling

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -6,9 +6,10 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div className="absolute top-0 left-0 w-screen aspect-video h-full -z-10 overflow-hidden">
+    <div className="absolute inset-0 -z-10 overflow-hidden">
       <iframe
-        className="absolute top-1/2 left-1/2 w-[120%] h-[120%] -translate-x-1/2 -translate-y-1/2"
+        className="absolute top-1/2 left-1/2 min-w-full min-h-full 
+                   w-auto h-auto -translate-x-1/2 -translate-y-1/2 scale-125"
         src={`https://www.youtube.com/embed/${trailerVideoKey}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideoKey}`}
         title="YouTube video player"
         allow="autoplay; encrypted-media"
