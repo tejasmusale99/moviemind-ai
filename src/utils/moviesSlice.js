7d4265080f6d3ea82a7a44dc8e4dc3f4635bd7f6
Tejas Musale <tejasmusale99@gmail.com>
2026-04-14 11:42:44 +0530

    updated movie slice for trailer action

--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -5,13 +5,17 @@ const moviesSlice = createSlice({
     name: "movies",
     initialState: {
         nowPlayingMovies:null,
+        trailer:null,
     },
     reducers: {
         addNowPlayingMovies: (state, action)=>{
             state.nowPlayingMovies = action.payload;
+        },
+        addTrailer : (state, action)=> {
+            state.trailer = action.payload;
         }
     }
 })
 
-export const { addNowPlayingMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailer } = moviesSlice.actions;
 export default moviesSlice.reducer;
