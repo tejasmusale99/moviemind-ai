564f6b58d97f49977e3bd6701f9030a37bc1d39a
Tejas Musale <tejasmusale99@gmail.com>
2026-05-10 13:17:06 +0530

    created secondary container for movie List

---
+++ src/components/SecondaryContainer.jsx
@@ -0,0 +1,9 @@
+const SecondaryContainer = () => {
+    return(
+        <div className="secondary-container">
+            <h1>Secondary Container</h1>
+        </div>  
+    )
+}
+
+export default SecondaryContainer;
